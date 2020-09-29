b82a72eedf92331c75ea596a2fd1ff7618a2969b
Jonathan Brownlee <jon.brownlee@hotmail.com>
2020-09-29 19:00:06 +1000

    UI working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import './App.css';
 
-function App() {
+// Taken from stackoverflow
+function getShuffledArr (arr){
+  return [...arr].map( (_, i, arrCopy) => {
+      var rand = i + ( Math.floor( Math.random() * (arrCopy.length - i) ) );
+      [arrCopy[rand], arrCopy[i]] = [arrCopy[i], arrCopy[rand]]
+      return arrCopy[i]
+  })
+}
 
+function App() {
   function sendEmail() {
-    let templateParams = {
-      to: "santaclausnoreply1@gmail.com",
-      message: 'Welcome to The Universe'
-    }
-    emailjs.send('default_service', 'template_bt5l82r', templateParams, 'user_S2B6oiUmBtw2Y5iTdBpK7')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    fetch("https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist&type=twopart&amount=5")
+      .then(res => res.json())
+      .then((data) => {
+        let jokes = data.jokes;
+        const shuffledEmailState = getShuffledArr(emailState);
+        shuffledEmailState.forEach((email, index) => {
+          let incrementedIndex = index + 1;
+          if (incrementedIndex === shuffledEmailState.length) {
+            incrementedIndex = 0;
+          }
+          const joke = jokes.pop()
+          const message = `Dear ${email.name}
+          
+          Merry Christmas!!!
+          
+          Your Secret Santa is ${shuffledEmailState[incrementedIndex].name}.
+          
+          ${joke.setup}
+          
+          ...
+
+          ${joke.delivery}
+
+          From Santa
+          `;
+          const templateParams = {
+            to: email.email,
+            messname: message
+          }
+
+          console.log(templateParams);
+          // emailjs.send('default_service', 'template_bt5l82r', templateParams, 'email_S2B6oiUmBtw2Y5iTdBpK7')
+          //   .then((result) => {
+          //     console.log(result.text);
+          //   }, (error) => {
+          //     console.log(error.text);
+          //   });
+        })
+      })
   }
+  const blankEmail = { email: '', name: '' };
+  const [emailState, setEmailState] = useState([
+    { ...blankEmail },
+  ]);
+
+  const addEmail = () => {
+    setEmailState([...emailState, { ...blankEmail }]);
+  };
+
+  const handleEmailChange = (e) => {
+    const updatedEmails = [...emailState];
+    updatedEmails[e.target.dataset.idx][e.target.className] = e.target.value;
+    setEmailState(updatedEmails);
+    console.log()
+  };
+
 
   return (
     <div>
-        <button onClick={sendEmail}>Primary</button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Secret Santa Sender</h1>
+      <form>
+        <input
+          type="button"
+          value="Add New Email"
+          onClick={addEmail}
+        />
+        {
+          emailState.map((val, idx) => {
+            const emailId = `email-${idx}`;
+            const nameId = `name-${idx}`;
+            return (
+              <div key={`email-${idx}`}>
+                <label htmlFor={emailId}>{`Email #${idx + 1}`}</label>
+                <input
+                  type="text"
+                  name={emailId}
+                  data-idx={idx}
+                  id={emailId}
+                  className="email"
+                  value={emailState[idx].email}
+                  onChange={handleEmailChange}
+                />
+                <label htmlFor={nameId}>Name</label>
+                <input
+                  type="text"
+                  name={nameId}
+                  data-idx={idx}
+                  id={nameId}
+                  className="name"
+                  value={emailState[idx].name}
+                  onChange={handleEmailChange}
+                />
+              </div>
+            );
+          })
+        }
+        <input type="button" value="Submit" onClick={sendEmail} />
+      </form>
     </div>
   );
 }
