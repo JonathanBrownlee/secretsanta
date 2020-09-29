97d769c16e02eee1422928139ef428c260658c12
Jonathan Brownlee <jon.brownlee@hotmail.com>
2020-09-29 15:51:46 +1000

    basic email service setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import emailjs from 'emailjs-com';
 import './App.css';
 
 function App() {
+
+  function sendEmail() {
+    let templateParams = {
+      to: "santaclausnoreply1@gmail.com",
+      message: 'Welcome to The Universe'
+    }
+    emailjs.send('default_service', 'template_bt5l82r', templateParams, 'user_S2B6oiUmBtw2Y5iTdBpK7')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+        <button onClick={sendEmail}>Primary</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
